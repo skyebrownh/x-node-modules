50c252f614a70ee2cc623ecb9d111fed01da234e
Skye Brown <skye.brownh@gmail.com>
2020-04-23 22:54:27 -0400

    recursive file traversing

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const fs = require('fs');
+const path = require('path');
 
 const buildTree = startPath => {
   // read directory contents of startPath
   fs.readdir(startPath, (err, entries) => {
-    console.log(entries);
     // iterate over directory contents of each startPath entry
-    entries.forEach(file => {
-      const path = `${startPath}/${file}`;
+    entries.forEach(entry => {
+      const newPath = path.join(startPath, entry);
 
       // recursively iterate over subdirectories
-      fs.lstat(path, (err, stats) => {
+      fs.lstat(newPath, (err, stats) => {
         if (stats.isDirectory()) {
-          buildTree(path);
+          // base case: delete node_modules
+          if (entry === 'node_modules') {
+            console.log(`delete ${newPath}`);
+          }
+          // recursive case: call buildTree on this directory
+          else {
+            buildTree(newPath);
+          }
         }
       });
     });
   });
 };
 
-buildTree('/Users/skyebrown/Documents/Projects/Web');
+buildTree('/Users/skyebrown/Documents');
